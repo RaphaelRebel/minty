20064f517adef463d7ffd7fc7c0569e13ce3a425
RaphaelRebel <raphaelrebel@live.com>
2025-08-14 23:02:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Gebruik de REST API om data op te halen/versturen
+Laat tenminste 10 verschillende tld's zien met prijzen (op basis van end-user input)
+In de web pagina aangeven of het domein beschikbaar is of niet.
+Maak een winkelwagen waar domeinen aan toegevoegd kunnen worden
+Wanneer tld niet beschikbaar is moet het niet mogelijk zijn om die toe te kunnen voegen aan de winkelmand
+Domeinen weer uit winkelmand kunnen halen
+Bereken het subtotaal + btw en laat deze op de checkout pagina zien.
+Voeg een bestelling toe aan een database (klik hier voor meer info)
+Een lijst met bestellingen.
+
+Bronnen:
+
+https://www.uptimia.com/questions/how-to-add-custom-headers-to-a-php-curl-request#:~:text=Tip%3A%20Handling%20Authorization%20Headers,ch%2C%20CURLOPT_HTTPHEADER%2C%20%24headers)%3B
+
+https://stackoverflow.com/questions/9802788/call-a-rest-api-in-php
+
+https://www.php.net/manual/en/function.curl-exec.php#:~:text=curl_exec(CurlHandle%20%24handle%20)%3A%20string,for%20the%20session%20are%20set.
+
+https://phppot.com/php/simple-php-shopping-cart/ 
+
+Ik heb last gehad van een bug dat de sessie een reset gaf. Dat bleek uitendelijk 
+  <?php session_abort(); ?> te zijn
---
+++ getDomains.php
@@ -0,0 +1,140 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+class GetDomains
+{
+
+         private $sessionActive = false;
+         //Get API info
+         private $apiUrl;
+         private $apiKey;
+         //Voeg parameters toe. In verband met tijd, zal ik het in het script stoppen.
+         private $params = array(
+                  'with_price' => true,
+         );
+         private $tdls = array(
+                  'com',
+                  'net',
+                  'org',
+                  'io',
+                  'co',
+                  'nl',
+                  'amsterdam',
+                  'dev',
+                  'shop',
+                  'info',
+         );
+
+         public function __construct()
+         {
+
+
+                  $this->apiUrl = $_ENV['API_URL'] ?? '';
+                  $this->apiKey = $_ENV['API_RULE'] . ' ' . $_ENV['API_KEY'] ?? '';
+                  if ($this->apiUrl === '' || $this->apiKey === '') {
+                           throw new RuntimeException('API_URL or API_KEY not set');
+                  }
+
+                  if (empty($_SESSION['domains'])) {
+                           $_SESSION['domains'] = [];
+                  }
+         }
+
+         public function callApi($method, $domain = 'rebootz')
+         {
+
+                  //Voeg hier de data toe.. Naam, extensions, ect..
+                  $data = array();
+
+                  foreach ($this->tdls as $tdl) {
+                           $data[] = array(
+                                    "name" => $domain,
+                                    "extension" => $tdl
+                           );
+                  }
+
+
+                  //start cUrl
+                  $curl = curl_init();
+
+                  //Voeg cURL opties toe (Voor nu, alleen POST..)
+                  switch ($method) {
+                           case 'POST':
+                                    curl_setopt($curl, CURLOPT_POST, 1);
+
+                                    // if($this->params){
+                                    //          curl_setopt($curl, CURLOPT_POSTFIELDS, $this->params);
+                                    // }
+
+                                    if ($data) {
+                                             curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+                                    }
+                                    break;
+                           default:
+                                    return json_encode(['error' => 'Wrong method applied.']);
+                  }
+
+
+                  //Add URL
+                  curl_setopt($curl, CURLOPT_URL, $this->apiUrl);
+                  //Add headers
+                  $headers = [
+                           'Authorization: ' . $this->apiKey,
+                           'Content-Type: application/json',
+                           'Accept: application/json',
+                  ];
+
+                  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+                  //Zorg ervoor dat de response wordt teruggegeven
+                  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                  $result = curl_exec($curl);
+
+
+
+
+                  return $result;
+
+         }
+
+         // private function storeAndReturnDomains($api, $domain)
+         // {
+         //          if (isset($_SESSION['domains'][$domain])) {
+         //                   return $_SESSION['domains'][$domain];
+         //          } else {
+         //                   $_SESSION['domains'][$domain] = $api;
+         //          }
+         //          return $_SESSION['domains'][$domain];
+         // }
+
+         public function getDomains($domain): bool|string|array
+         {
+                  try {
+                           if (session_status() !== PHP_SESSION_ACTIVE) {
+
+                                    session_start();
+                           }
+
+                           // var_dump(`'SID', session_id(), array_keys($_SESSION['domains'] ?? []));
+                           if ($_SESSION['domains'][$domain] ?? '' !== '') {
+                                    return $_SESSION['domains'][$domain];
+                           }
+                           $json = $this->callApi('POST', $domain);
+                           $data = json_decode($json, true);
+                           $_SESSION['domains'][$domain] = $data;
+                           if (json_last_error() !== JSON_ERROR_NONE) {
+                                    throw new RuntimeException('Invalid JSON from API: ' . json_last_error_msg());
+                           }
+
+
+                           // if you immediately redirect after this in your controller:
+                           // session_write_close();
+
+                           return $data;
+
+                  } catch (Exception $e) {
+                           // Handle exception
+                           return json_encode(['error' => 'An error occurred while fetching domains: ' . $e->getMessage()]);
+                  }
+         }
+
+}
